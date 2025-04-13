33b12e5a04f078cfc4b798ab4b2fdc475a3f51ee
Duskitten <theduskitty@gmail.com>
2025-04-12 22:07:23 -0400

    Add dropper system

---
+++ addons/Cubiix_Assets/Mods/TTSAsset/Assets/Scripts/Client/Tools/Dropper.gd
@@ -0,0 +1,36 @@
+extends Control
+
+func _ready():
+	get_window().files_dropped.connect(on_files_dropped)
+	print("Ok")
+	
+	
+func on_files_dropped(files):
+	for i in files:
+		if i.ends_with(".png"):
+			var template = $Panel.duplicate(true)
+			template.texture = load(i)
+			$ScrollContainer/GridContainer.add_child(template)
+			template.show()
+	await get_tree().process_frame
+	$Topbar/HBoxContainer/ExportImageSize.text = "Export Image Size: "+ str(Vector2i($ScrollContainer/GridContainer.get_rect().size))
+	
+		
+
+func _on_spin_box_value_changed(value: float) -> void:
+	var newval = int(value)
+	$ScrollContainer/GridContainer.columns = newval
+	await get_tree().process_frame
+	$Topbar/HBoxContainer/ExportImageSize.text = "Export Image Size: "+ str(Vector2i($ScrollContainer/GridContainer.get_rect().size))
+	
+		
+
+
+func _on_button_2_pressed() -> void:
+	$SubViewport.size = $ScrollContainer/GridContainer.get_rect().size
+	var newimages:GridContainer = $ScrollContainer/GridContainer.duplicate(true)
+	newimages.position = Vector2.ZERO
+	$SubViewport/Control.add_child(newimages)
+	await RenderingServer.frame_post_draw
+	$SubViewport.get_texture().get_image().save_png("user://beep.png") 
+	OS.shell_open(ProjectSettings.globalize_path("user://"))
