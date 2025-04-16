ae257c3087959c0b2b02790b4aad94390dddbb07
Duskitten <theduskitty@gmail.com>
2025-04-15 22:03:25 -0400

    Added U.R., moved Dropper system, updated U.I.

--- addons/Cubiix_Assets/Mods/TTSAsset/Assets/Scripts/Client/Tools/Dropper.gd
+++ addons/Cubiix_Assets/Mods/TTSAsset/Assets/Scripts/Client/Tools/Dropper.gd
@@ -8,8 +8,12 @@ func _ready():
 func on_files_dropped(files):
 	for i in files:
 		if i.ends_with(".png"):
+			print(i)
 			var template = $Panel.duplicate(true)
-			template.texture = load(i)
+			var img = Image.new()
+			var texture = ImageTexture.new()
+			img.load(i)
+			template.texture = texture.create_from_image(img)
 			$ScrollContainer/GridContainer.add_child(template)
 			template.show()
 	await get_tree().process_frame
@@ -27,10 +31,10 @@ func _on_spin_box_value_changed(value: float) -> void:
 
 
 func _on_button_2_pressed() -> void:
-	#$SubViewport.size = $ScrollContainer/GridContainer.get_rect().size
-	#var newimages:GridContainer = $ScrollContainer/GridContainer.duplicate(true)
-	#newimages.position = Vector2.ZERO
-	#$SubViewport/Control.add_child(newimages)
-	#await RenderingServer.frame_post_draw
-	#$SubViewport.get_texture().get_image().save_png("user://beep.png") 
+	$SubViewport.size = $ScrollContainer/GridContainer.get_rect().size
+	var newimages:GridContainer = $ScrollContainer/GridContainer.duplicate(true)
+	newimages.position = Vector2.ZERO
+	$SubViewport/Control.add_child(newimages)
+	await RenderingServer.frame_post_draw
+	$SubViewport.get_texture().get_image().save_png("user://beep.png") 
 	OS.shell_open(ProjectSettings.globalize_path("user://"))
